1fe8140e838d8145dbfd61903dc3f8437e3bf576
javariaimtiaz <javaria.imtiaz@nu.edu.pk>
2021-10-15 13:04:20 +0500

    Another day another commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,7 +6,7 @@ public class Test {
 		
 		System.out.println("Greetings dear students");
 		System.out.println("Welcome to CI/CD");
-		System.out.println("An important phase of DevOps");
+		System.out.println("An Important phase of DevOps");
 
 	}
 
